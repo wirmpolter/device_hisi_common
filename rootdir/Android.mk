16823a9551b47e85aa69042f54aa97bc191cca2b
wirmpolter <wirmpolter@gmail.com>
2018-06-27 22:57:36 -0400

    mha: rootdir: fix wrong filename

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -13,6 +13,6 @@ LOCAL_MODULE       := init.hi3660.environ.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_POST_INSTALL_CMD := \
     mkdir -p $(PRODUCT_OUT)/system/etc/init && \
-    sed -e 's?%BOOTCLASSPATH%?$(PRODUCT_BOOTCLASSPATH)?g' $(LOCAL_PATH)/etc/init.hi3660.environ.rc > $(PRODUCT_OUT)/system/etc/init/init.kirin970.environ.rc && \
+    sed -e 's?%BOOTCLASSPATH%?$(PRODUCT_BOOTCLASSPATH)?g' $(LOCAL_PATH)/etc/init.hi3660.environ.rc > $(PRODUCT_OUT)/system/etc/init/init.hi3660.environ.rc && \
     sed -i -e 's?%SYSTEMSERVERCLASSPATH%?$(PRODUCT_SYSTEM_SERVER_CLASSPATH)?g' $(PRODUCT_OUT)/system/etc/init/init.hi3660.environ.rc
 include $(BUILD_PHONY_PACKAGE)
