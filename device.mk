ea75471cd6abc6677aa294916019612635e52965
wirmpolter <wirmpolter@gmail.com>
2018-07-13 21:29:42 -0400

    mha: build hidl.base and manager

--- device.mk
+++ device.mk
@@ -42,6 +42,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/compatibility_matrix.xml:system/compatibility_matrix.xml
 
+PRODUCT_PACKAGES += \
+    android.hidl.base@1.0 \
+    android.hidl.manager@1.0
+
 # Input
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/fingerprint.kl:system/usr/keylayout/fingerprint.kl
