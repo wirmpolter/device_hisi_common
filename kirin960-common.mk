af0e8802b3a2b6990594eccd7a0b9e7684aeb34b
Flex1911 <dedsa2002@gmail.com>
2018-08-16 16:36:17 -0400

    kirin960-common: Update ld.config for vndk 26

--- kirin960-common.mk
+++ kirin960-common.mk
@@ -131,4 +131,6 @@ PRODUCT_PACKAGES += \
 
 # VNDK
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/vndk-compat/ld.config.compat.txt:system/etc/ld.config.compat.txt
+    $(LOCAL_PATH)/vndk-compat/ld.config.26.txt:system/etc/ld.config.26.txt \
+    $(LOCAL_PATH)/vndk-compat/ld.config.27.txt:system/etc/ld.config.27.txt
+
